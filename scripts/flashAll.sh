574f46770282e1b2aa756c6913e71a1b2dd70f0e
Wolfgang Hoenig <whoenig@usc.edu>
2016-07-17 22:55:16 -0700

    update crazyswarm launch files

--- scripts/flashAll.sh
+++ scripts/flashAll.sh
@@ -1,8 +1,11 @@
 # ../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E701 flash ../crazyflie2-nrf-firmware/cf2_nrf.bin nrf51-fw
 # ../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E701 flash ../crazyflie-firmware/cf2.bin stm32-fw
 
-../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E702 flash ../crazyflie2-nrf-firmware/cf2_nrf.bin nrf51-fw
-../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E702 flash ../crazyflie-firmware/cf2.bin stm32-fw
+# ../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E703 flash ../crazyflie2-nrf-firmware/cf2_nrf.bin nrf51-fw
 
-../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E703 flash ../crazyflie2-nrf-firmware/cf2_nrf.bin nrf51-fw
 ../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E703 flash ../crazyflie-firmware/cf2.bin stm32-fw
+../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E705 flash ../crazyflie-firmware/cf2.bin stm32-fw
+../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E707 flash ../crazyflie-firmware/cf2.bin stm32-fw
+
+# ../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E705 flash ../crazyflie2-nrf-firmware/cf2_nrf.bin nrf51-fw
+../crazyflie-clients-python/bin/cfloader -w radio://0/100/2M/E7E7E7E709 flash ../crazyflie-firmware/cf2.bin stm32-fw

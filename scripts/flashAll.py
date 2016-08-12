b741d310fa1e195906c672932bcc879460c7de24
James Preiss <japreiss@usc.edu>
2016-08-11 19:05:58 -0700

    fix flashAll help msg

--- scripts/flashAll.py
+++ scripts/flashAll.py
@@ -5,11 +5,15 @@ import subprocess
 import argparse
 import os
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description='Flash firmware.')
     parser.add_argument('-stm32', action="store_true")
     parser.add_argument('-nrf51', action="store_true")
     args = parser.parse_args()
+
+    if not (args.nrf51 or args.stm32):
+        print("-stm32, -nrf51, or both?")
+        return
 
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
@@ -22,8 +26,10 @@ if __name__ == "__main__":
         if args.nrf51:
             print("Flash NRF51 FW to {}".format(uri))
             subprocess.call(["../crazyflie-clients-python/bin/cfloader -w " + uri + " flash ../crazyflie2-nrf-firmware/cf2_nrf.bin nrf51-fw"], shell=True)
-        elif args.stm32:
+        if args.stm32:
             print("Flash STM32 FW to {}".format(uri))
             subprocess.call(["../crazyflie-clients-python/bin/cfloader -w " + uri + " flash ../crazyflie-firmware/cf2.bin stm32-fw"], shell=True)
-        else:
-            print("-stm32 or -nrf51?")
+
+
+if __name__ == "__main__":
+    main()

89180ffa132c36fc4da85b7151b8cc5dc743c80e
Wolfgang Hoenig <whoenig@usc.edu>
2016-08-26 18:52:42 -0700

    update submodules

---
+++ scripts/version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import yaml
+import subprocess
+import os
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+with open("../ros_ws/src/crazyswarm/launch/crazyflies.yaml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+for crazyflie in cfg["crazyflies"]:
+    id = "{0:02X}".format(crazyflie["id"])
+    uri = "radio://0/{}/2M/E7E7E7E7{}".format(crazyflie["channel"], id)
+    print("{}: ".format(crazyflie["id"]), end="", flush=True)
+    subprocess.call(["rosrun crazyflie_tools version --uri " + uri], shell=True)

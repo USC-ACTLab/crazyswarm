a6ce8ee928482e381a2747d04875960136a34263
Mark Debord <mjdebord@usc.edu>
2017-11-02 16:06:21 -0700

    hybrid bigQuad and regular CF

---
+++ ros_ws/src/crazyswarm/scripts/testDownwashVertical.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import numpy as np
+import math
+
+from pycrazyswarm import *
+import pycrazyswarm.cfsim.cffirmware as firm
+
+if __name__ == "__main__":
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs
+
+    ids = [2,14]
+    cfs = [allcfs.crazyfliesById[i] for i in ids]
+
+    print("press button to take off...")
+    swarm.input.waitUntilButtonPressed()
+
+    cfs[0].takeoff(targetHeight=2.0, duration=4.0)
+    cfs[1].takeoff(targetHeight=0.5, duration=1.5)
+
+    timeHelper.sleep(4.5)
+
+    print("press button to move on top")
+    swarm.input.waitUntilButtonPressed()
+
+    cfs[0].hover(np.array([0, 0, 2.0]), 0, 1.0)
+    cfs[1].hover(np.array([0, 1, 0.5]), 0, 4.0)
+
+    timeHelper.sleep(3.5)
+
+    pos = np.array([0, 0, 0.5])
+    while True:
+        print("current pos", pos)
+        print("press left/right shoulder to move up/down")
+        buttons = swarm.input.waitUntilAnyButtonPressed()
+        print(buttons)
+        if buttons[5] == 1:
+            pos += np.array([0, 0.0, 0.05])
+        elif buttons[4] == 1:
+            pos -= np.array([0, 0.0, 0.05])
+        elif buttons[3] == 1:
+            break
+        if pos[1] == 1:
+            pos[1] = -1
+        else:
+            pos[1] = 1
+        cfs[1].hover(pos, 0, 4.0)
+
+    cfs[1].hover(cfs[1].initialPosition + np.array([0, 0, 0.5]), 0, 4.0)
+    timeHelper.sleep(4.5)
+
+    cfs[0].land(targetHeight=0.02, duration=4.0)
+    cfs[1].land(targetHeight=0.02, duration=1.5)
+    timeHelper.sleep(4.0)

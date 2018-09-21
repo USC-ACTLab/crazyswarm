fa8958c8ea3c025aa41e9db2a24affe1417d0750
Wolfgang Hoenig <whoenig@usc.edu>
2018-09-21 14:26:29 -0700

    add spiral example

---
+++ ros_ws/src/crazyswarm/scripts/spiral.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from pycrazyswarm import *
+import uav_trajectory
+
+if __name__ == "__main__":
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs
+
+    traj1 = uav_trajectory.Trajectory()
+    traj1.loadcsv("spiral.csv")
+
+    TRIALS = 1
+    TIMESCALE = 1.0
+    REPEAT = 3
+    for i in range(TRIALS):
+        for cf in allcfs.crazyflies:
+            cf.uploadTrajectory(0, 0, traj1)
+
+        allcfs.takeoff(targetHeight=1.0, duration=2.0)
+        timeHelper.sleep(2.5)
+        for cf in allcfs.crazyflies:
+            pos = np.array(cf.initialPosition) + np.array([0, 0, 1.0])
+            cf.goTo(pos, 0, 2.0)
+        timeHelper.sleep(2.5)
+
+        for j in range(REPEAT):
+            allcfs.startTrajectory(0, timescale=TIMESCALE)
+            timeHelper.sleep(traj1.duration * TIMESCALE)
+        # allcfs.startTrajectory(0, timescale=TIMESCALE, reverse=True)
+        # timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
+
+        allcfs.land(targetHeight=0.06, duration=2.0)
+        timeHelper.sleep(3.0)
+

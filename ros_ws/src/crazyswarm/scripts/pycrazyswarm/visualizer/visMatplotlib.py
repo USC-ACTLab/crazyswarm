2f5a8b5cece235df1fef7c52c1c211bee4cdd392
Barcis, Michal <michal.barcis@aau.at>
2019-08-08 11:40:10 +0200

    added color to matplotlib visualization

--- ros_ws/src/crazyswarm/scripts/pycrazyswarm/visualizer/visMatplotlib.py
+++ ros_ws/src/crazyswarm/scripts/pycrazyswarm/visualizer/visMatplotlib.py
@@ -1,4 +1,4 @@
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D  # NOQA
 import matplotlib.pyplot as plt
 
 
@@ -6,9 +6,9 @@ class VisMatplotlib:
     def __init__(self):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
-        self.ax.set_xlim([-5,5])
-        self.ax.set_ylim([-5,5])
-        self.ax.set_zlim([0,3])
+        self.ax.set_xlim([-5, 5])
+        self.ax.set_ylim([-5, 5])
+        self.ax.set_zlim([0, 3])
         self.ax.set_xlabel("X")
         self.ax.set_ylabel("Y")
         self.ax.set_zlabel("Z")
@@ -19,16 +19,23 @@ class VisMatplotlib:
         xs = []
         ys = []
         zs = []
+        cs = []
         for cf in crazyflies:
             x, y, z = cf.position()
+            color = cf.ledRGB
             xs.append(x)
             ys.append(y)
             zs.append(z)
+            cs.append(color)
 
         if self.plot is None:
-            self.plot = self.ax.scatter(xs, ys, zs)
+            self.plot = self.ax.scatter(xs, ys, zs, c=cs)
         else:
             self.plot._offsets3d = (xs, ys, zs)
+            self.plot.set_facecolors(cs)
+            self.plot.set_edgecolors(cs)
+            self.plot._facecolor3d = self.plot.get_facecolor()
+            self.plot._edgecolor3d = self.plot.get_edgecolor()
 
         self.timeAnnotation.set_text("{} s".format(t))
         plt.pause(0.0001)

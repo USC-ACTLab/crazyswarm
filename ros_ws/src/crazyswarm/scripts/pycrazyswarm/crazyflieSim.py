49bcfbb650a9321f897945daafa73bbc8128942e
jpreiss <jamesalanpreiss@gmail.com>
2020-01-13 16:47:21 -0800

    Option to load launch YAML from string or alternate path.
    Until now, the set of robots in a pycrazyswarm script is always
    determined by ros_ws/src/crazyswarm/launch/crazyflies.yaml. This
    couples the result of the script to the state of the executing machine.
    If we want to add more automated testing in the future, it should be
    possible to write a pycrazyswarm script that runs deterministically
    without the cumbersome process of writing .yaml to disk.

--- ros_ws/src/crazyswarm/scripts/pycrazyswarm/crazyflieSim.py
+++ ros_ws/src/crazyswarm/scripts/pycrazyswarm/crazyflieSim.py
@@ -219,9 +219,20 @@ class Crazyflie:
 
 
 class CrazyflieServer:
-    def __init__(self, timeHelper):
-        with open("../launch/crazyflies.yaml", 'r') as ymlfile:
-            cfg = yaml.load(ymlfile)
+    def __init__(self, timeHelper, launch_file="../launch/crazyflies.yaml"):
+        """Initialize the server.
+
+        Args:
+            timeHelper (TimeHelper): TimeHelper instance.
+            launch_file (str): If ends in ".yaml", interpret as a path and load
+                from file. Otherwise, interpret as YAML string and parse
+                directly from string.
+        """
+        if launch_file.endswith(".yaml"):
+            with open(launch_file, 'r') as ymlfile:
+                cfg = yaml.load(ymlfile)
+        else:
+            cfg = yaml.load(launch_file)
 
         self.crazyflies = []
         self.crazyfliesById = dict()
--- ros_ws/src/crazyswarm/scripts/pycrazyswarm/crazyswarm.py
+++ ros_ws/src/crazyswarm/scripts/pycrazyswarm/crazyswarm.py
@@ -16,17 +16,22 @@ def build_argparser():
 
 
 class Crazyswarm:
-    def __init__(self):
+    def __init__(self, launch_yaml=None):
         parser = build_argparser()
         args, unknown = parser.parse_known_args()
+
+        if launch_yaml is None:
+            launch_yaml = "../launch/crazyflies.yaml"
+        if launch_yaml.endswith(".yaml"):
+            launch_yaml = open(launch_yaml, 'r').read()
 
         if args.sim:
             import crazyflieSim
             self.timeHelper = crazyflieSim.TimeHelper(args.vis, args.dt, args.writecsv)
-            self.allcfs = crazyflieSim.CrazyflieServer(self.timeHelper)
+            self.allcfs = crazyflieSim.CrazyflieServer(self.timeHelper, launch_yaml)
         else:
             import crazyflie
-            self.allcfs = crazyflie.CrazyflieServer()
+            self.allcfs = crazyflie.CrazyflieServer(launch_yaml)
             self.timeHelper = crazyflie.TimeHelper()
             if args.writecsv:
                 print("WARNING: writecsv argument ignored! This is only available in simulation.")

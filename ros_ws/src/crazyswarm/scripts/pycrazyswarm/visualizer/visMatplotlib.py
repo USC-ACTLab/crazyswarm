d9629cf8bdb2fde524989e04246fe6afbd83d0b0
jpreiss <jamesalanpreiss@gmail.com>
2020-01-13 16:26:41 -0800

    Ability to draw communication graph in sim visualizer.

--- ros_ws/src/crazyswarm/scripts/pycrazyswarm/visualizer/visMatplotlib.py
+++ ros_ws/src/crazyswarm/scripts/pycrazyswarm/visualizer/visMatplotlib.py
@@ -1,5 +1,7 @@
-from mpl_toolkits.mplot3d import Axes3D  # NOQA
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class VisMatplotlib:
@@ -14,6 +16,27 @@ class VisMatplotlib:
         self.ax.set_zlabel("Z")
         self.plot = None
         self.timeAnnotation = self.ax.annotate("Time", xy=(0, 0), xycoords='axes fraction', fontsize=12, ha='right', va='bottom')
+
+        self.line_color = 0.3 * np.ones(3)
+
+        # Lazy-constructed data for connectivity graph gfx.
+        self.graph_edges = None
+        self.graph_lines = None
+        self.graph = None
+
+    def setGraph(self, edges):
+        """Set edges of graph visualization - sequence of (i,j) tuples."""
+
+        # Only allocate new memory if we need to.
+        n_edges = len(edges)
+        if self.graph_edges is None or n_edges != len(self.graph_edges):
+            self.graph_lines = np.zeros((n_edges, 2, 3))
+        self.graph_edges = edges
+
+        # Lazily construct Matplotlib object for graph.
+        if self.graph is None:
+            self.graph = Line3DCollection(self.graph_lines, edgecolor=self.line_color)
+            self.ax.add_collection(self.graph)
 
     def update(self, t, crazyflies):
         xs = []
@@ -31,11 +54,19 @@ class VisMatplotlib:
         if self.plot is None:
             self.plot = self.ax.scatter(xs, ys, zs, c=cs)
         else:
+            # TODO: Don't use protected members.
             self.plot._offsets3d = (xs, ys, zs)
             self.plot.set_facecolors(cs)
             self.plot.set_edgecolors(cs)
             self.plot._facecolor3d = self.plot.get_facecolor()
             self.plot._edgecolor3d = self.plot.get_edgecolor()
+
+        if self.graph is not None:
+            # Update graph line segments to match new Crazyflie positions.
+            for k, (i, j) in enumerate(self.graph_edges):
+                self.graph_lines[k, 0, :] = xs[i], ys[i], zs[i]
+                self.graph_lines[k, 1, :] = xs[j], ys[j], zs[j]
+                self.graph.set_segments(self.graph_lines)
 
         self.timeAnnotation.set_text("{} s".format(t))
         plt.pause(0.0001)

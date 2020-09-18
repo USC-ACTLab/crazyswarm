65749729a83d9c04e507bcc32a887eb63b3d82a6
jpreiss <jamesalanpreiss@gmail.com>
2020-09-18 01:57:33 -0700

    targetyaw=0 for updated fw takeoff/land

--- ros_ws/src/crazyswarm/scripts/pycrazyswarm/crazyflieSim.py
+++ ros_ws/src/crazyswarm/scripts/pycrazyswarm/crazyflieSim.py
@@ -113,13 +113,13 @@ class Crazyflie:
         if self._isGroup(groupMask):
             self.mode = Crazyflie.MODE_HIGH_POLY
             firm.plan_takeoff(self.planner,
-                self.state.pos, self.state.yaw, targetHeight, duration, self.time())
+                self.state.pos, self.state.yaw, targetHeight, 0.0, duration, self.time())
 
     def land(self, targetHeight, duration, groupMask = 0):
         if self._isGroup(groupMask):
             self.mode = Crazyflie.MODE_HIGH_POLY
             firm.plan_land(self.planner,
-                self.state.pos, self.state.yaw, targetHeight, duration, self.time())
+                self.state.pos, self.state.yaw, targetHeight, 0.0, duration, self.time())
 
     def stop(self, groupMask = 0):
         if self._isGroup(groupMask):

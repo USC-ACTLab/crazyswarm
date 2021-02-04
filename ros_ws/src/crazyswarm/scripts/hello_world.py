38e3bfcd78d5b048d5916bda6ceae04282f48d2c
jpreiss <jamesalanpreiss@gmail.com>
2021-02-03 20:14:15 -0800

    add hello_world script

---
+++ ros_ws/src/crazyswarm/scripts/hello_world.py
@@ -0,0 +1,22 @@
+"""Takeoff-hover-land for one CF. Useful to validate hardware config."""
+
+from pycrazyswarm import Crazyswarm
+
+
+TAKEOFF_DURATION = 2.5
+HOVER_DURATION = 5.0
+
+
+def main():
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    cf = swarm.allcfs.crazyflies[0]
+
+    cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+    timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
+    cf.land(targetHeight=0.04, duration=2.5)
+    timeHelper.sleep(TAKEOFF_DURATION)
+
+
+if __name__ == "__main__":
+    main()

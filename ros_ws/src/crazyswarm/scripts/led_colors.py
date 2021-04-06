af91995e464edc165d0fd06bef54f438884e796e
jpreiss <jamesalanpreiss@gmail.com>
2021-04-06 12:23:38 -0700

    force LED ring mode to solid in led_colors example

--- ros_ws/src/crazyswarm/scripts/led_colors.py
+++ ros_ws/src/crazyswarm/scripts/led_colors.py
@@ -10,7 +10,12 @@ if __name__ == "__main__":
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
 
-    # generate all possible on/off rgb values
+    # Configure the CFs so that the LED ring displays the solid color.
+    # Overrides the launch file and the firmware default.
+    for cf in allcfs.crazyflies:
+        cf.setParam("ring/effect", 7)
+
+    # Generate all possible on/off rgb values.
     rgb_bits = [tuple((x >> k) & 0x1 for k in range(3)) for x in range(8)]
 
     allcfs.takeoff(targetHeight=1.0, duration=2.0)

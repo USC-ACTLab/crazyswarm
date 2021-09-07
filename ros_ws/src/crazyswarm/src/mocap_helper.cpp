f9bc95d7a7cc9121958d2c5fb5914caf52d07121
Wolfgang Hoenig <whoenig@caltech.edu>
2021-09-07 17:57:43 +0200

    Switch libmotioncapture to https://github.com/IMRCLab/libmotioncapture
    The updated version of libmotioncapture has a refined API, Python
    bindings, factory method to avoid repetitive code, and improved
    OptiTrack support.

--- ros_ws/src/crazyswarm/src/mocap_helper.cpp
+++ ros_ws/src/crazyswarm/src/mocap_helper.cpp
@@ -4,21 +4,7 @@
 #include <ros/ros.h>
 
 // Motion Capture
-#ifdef ENABLE_VICON
-#include <libmotioncapture/vicon.h>
-#endif
-#ifdef ENABLE_OPTITRACK
-#include <libmotioncapture/optitrack.h>
-#endif
-#ifdef ENABLE_PHASESPACE
-#include <libmotioncapture/phasespace.h>
-#endif
-#ifdef ENABLE_QUALISYS
-#include <libmotioncapture/qualisys.h>
-#endif
-#ifdef ENABLE_VRPN
-#include <libmotioncapture/vrpn.h>
-#endif
+#include <libmotioncapture/motioncapture.h>
 
 int main(int argc, char **argv)
 {
@@ -29,98 +15,42 @@ int main(int argc, char **argv)
   std::string motionCaptureType;
   nl.param<std::string>("motion_capture_type", motionCaptureType, "vicon");
 
-  // Make a new client
-  libmotioncapture::MotionCapture* mocap = nullptr;
-  if (false)
-  {
-  }
-#ifdef ENABLE_VICON
-  else if (motionCaptureType == "vicon")
-  {
-    std::string hostName;
-    nl.getParam("vicon_host_name", hostName);
-    mocap = new libmotioncapture::MotionCaptureVicon(hostName,
-      /*enableObjects*/ true,
-      /*enablePointcloud*/ true);
-  }
-#endif
-#ifdef ENABLE_OPTITRACK
-  else if (motionCaptureType == "optitrack")
-  {
-    std::string hostName;
-    nl.getParam("optitrack_host_name", hostName);
-    mocap = new libmotioncapture::MotionCaptureOptitrack(hostName);
-  }
-#endif
-#ifdef ENABLE_PHASESPACE
-  else if (motionCaptureType == "phasespace")
-  {
-    std::string ip;
-    int numMarkers;
-    nl.getParam("phasespace_ip", ip);
-    nl.getParam("phasespace_num_markers", numMarkers);
-    std::map<size_t, std::pair<int, int> > cfs;
-    cfs[231] = std::make_pair<int, int>(10, 11);
-    mocap = new libmotioncapture::MotionCapturePhasespace(ip, numMarkers, cfs);
-  }
-#endif
-#ifdef ENABLE_QUALISYS
-  else if (motionCaptureType == "qualisys")
-  {
-    std::string hostname;
-    int port;
-    nl.getParam("qualisys_host_name", hostname);
-    nl.getParam("qualisys_base_port", port);
-    mocap = new libmotioncapture::MotionCaptureQualisys(hostname, port,
-      /*enableObjects*/ true,
-      /*enablePointcloud*/ true);
-  }
-#endif
-#ifdef ENABLE_VRPN
-  else if (motionCaptureType == "vrpn")
-  {
-    std::string hostname;
-    int port;
-    nl.getParam("vrpn_host_name", hostname);
-    mocap = new libmotioncapture::MotionCaptureVrpn(hostname);
-  }
-#endif
-  else {
+  std::map<std::string, std::string> cfg;
+  std::string hostname;
+  nl.getParam("motion_capture_host_name", hostname);
+  cfg["hostname"] = hostname;
+  libmotioncapture::MotionCapture *mocap = libmotioncapture::MotionCapture::connect(motionCaptureType, cfg);
+  if (!mocap) {
     throw std::runtime_error("Unknown motion capture type!");
   }
-
-  pcl::PointCloud<pcl::PointXYZ>::Ptr markers(new pcl::PointCloud<pcl::PointXYZ>);
-  std::vector<libmotioncapture::Object> objects;
 
   for (size_t frameId = 0; ros::ok(); ++frameId) {
     std::cout << "frame " << frameId << ":" << std::endl;
     // Get a frame
     mocap->waitForNextFrame();
-    mocap->getPointCloud(markers);
+    const auto& markers = mocap->pointCloud();
 
     std::cout << "    points:" << std::endl;
 
-    for (size_t i = 0; i < markers->size(); ++i) {
-      const pcl::PointXYZ& point = markers->at(i);
-      std::cout << "      \"" << i << "\": [" << point.x << "," << point.y << "," << point.z << "]" << std::endl;
+    for (size_t i = 0; i < markers.rows(); ++i) {
+      const auto& point = markers.row(i);
+      std::cout << "      \"" << i << "\": [" << point(0) << "," << point(1) << "," << point(2) << "]" << std::endl;
     }
 
-    if (mocap->supportsObjectTracking()) {
-      mocap->getObjects(objects);
+    if (mocap->supportsRigidBodyTracking()) {
+      const auto& rigidBodies = mocap->rigidBodies();
 
-      std::cout << "    objects:" << std::endl;
+      std::cout << "    rigidBodies:" << std::endl;
 
-      for (auto const& object: objects) {
-        std::cout << "      \"" << object.name() << "\":" << std::endl;
-        std::cout << "         occluded: " << object.occluded() << std::endl;
+      for (auto const& kv: rigidBodies) {
+        const auto& body = kv.second;
+        std::cout << "      \"" << body.name() << "\":" << std::endl;
 
-        if (object.occluded() == false) {
-          Eigen::Vector3f position = object.position();
-          Eigen::Quaternionf rotation = object.rotation();
-          std::cout << "         position: [" << position(0) << ", " << position(1) << ", " << position(2) << "]" << std::endl;
-          std::cout << "         rotation: [" << rotation.w() << ", " << rotation.vec()(0) << ", "
-                                              << rotation.vec()(1) << ", " << rotation.vec()(2) << "]" << std::endl;
-        }
+        Eigen::Vector3f position = body.position();
+        Eigen::Quaternionf rotation = body.rotation();
+        std::cout << "       position: [" << position(0) << ", " << position(1) << ", " << position(2) << "]" << std::endl;
+        std::cout << "       rotation: [" << rotation.w() << ", " << rotation.vec()(0) << ", "
+                                          << rotation.vec()(1) << ", " << rotation.vec()(2) << "]" << std::endl;
       }
     }
 

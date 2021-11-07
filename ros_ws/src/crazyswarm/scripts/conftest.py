bc47dc0d13a6d67f426f89bb405d5351062286bd
James Preiss <japreiss@usc.edu>
2021-11-07 11:28:12 -0800

    add print debugging for simROS hang

--- ros_ws/src/crazyswarm/scripts/conftest.py
+++ ros_ws/src/crazyswarm/scripts/conftest.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import subprocess
 
 import pytest
@@ -21,28 +22,35 @@ def pytest_addoption(parser):
 @pytest.fixture
 def crazyswarm_ctor(pytestconfig):
     if pytestconfig.getoption("simros"):
-        assert HAVE_ROS, "cannot run --simros tests without ROS installed."
         # ROS simulator - start CSW server first
+        assert HAVE_ROS, "cannot run --simros tests without ROS installed."
+        # TODO Once we drop Python 2, this by-reference hack should be replaced
+        # with a scalar variable and a `nonlocal` statement inside the closure.
         ros_process = [None]
+        print("Defining ctor")
         def ctor(**kwargs):
             assert "crazyflies_yaml" in kwargs
             # TODO: It might be cleaner to use some kind of filesystem mocking,
             # but does it matter?
+            print("Writing YAML")
             with open("../launch/crazyflies.yaml", "w") as f:
                 f.write(kwargs["crazyflies_yaml"])
-            ros_process[0] = subprocess.Popen([
-                "roslaunch",
-                "crazyswarm",
-                "hover_swarm.launch",
-                "--sim"
-            ])
+            print("Spawning ROS")
+            ros_process[0] = subprocess.Popen(
+                ["roslaunch", "crazyswarm", "hover_swarm.launch", "--sim"],
+                shell=True
+            )
+            print("Spawned ROS")
             return Crazyswarm(**kwargs)
+        print("Yielding ctor")
         yield ctor
+        print("Done with yield")
         # Control will NOT return here immediately after the test calls
         # `ctor()`. Instead, pytest remembers that the fixture is in `yield`
         # state and returns control _after the test finishes_. For details, see
         # https://docs.pytest.org/en/latest/how-to/fixtures.html#yield-fixtures-recommended
         ros_process[0].terminate()
+        print("Terminated")
     else:
         # Pycrazyswarm simulator
         def ctor(**kwargs):

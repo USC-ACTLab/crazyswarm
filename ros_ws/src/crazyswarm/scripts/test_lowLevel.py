0b3b6a084061c091ad775f8721091339467842cb
jpreiss <jamesalanpreiss@gmail.com>
2021-11-08 16:38:53 -0800

    simROS: repeat cmds in low-level tests, integration fudge

--- ros_ws/src/crazyswarm/scripts/test_lowLevel.py
+++ ros_ws/src/crazyswarm/scripts/test_lowLevel.py
@@ -29,41 +29,55 @@ def test_cmdFullState_zeroVel(setUp):
     cf = allcfs.crazyflies[0]
 
     pos = np.array(cf.initialPosition) + np.array([1, 1, Z])
-    cf.cmdFullState(pos, np.zeros(3), np.zeros(3), 0, np.zeros(3))
-    timeHelper.sleep(1.0)
+    for _ in range(10):
+        cf.cmdFullState(pos, np.zeros(3), np.zeros(3), 0, np.zeros(3))
+        timeHelper.sleep(0.1)
 
     assert np.all(np.isclose(cf.position(), pos))
+
 
 def test_cmdPosition(setUp):
     allcfs, timeHelper = setUp()
     cf = allcfs.crazyflies[0]
 
     pos = np.array(cf.initialPosition) + np.array([1, 1, Z])
-    cf.cmdPosition(pos,yaw=0.0)
-    timeHelper.sleep(1.0)
+    for _ in range(10):
+        cf.cmdPosition(pos, yaw=0.0)
+        timeHelper.sleep(0.1)
 
     assert np.all(np.isclose(cf.position(), pos))
 
+
+@pytest.mark.sync  # velocity() method doesn't exist on hardware.
 def test_cmdVelocityWorld_checkVelocity(setUp):
     allcfs, timeHelper = setUp()
-    
+
     cf = allcfs.crazyflies[0]
     vel = np.ones(3)
     cf.cmdVelocityWorld(vel, yawRate=0)
     timeHelper.sleep(1.0)
 
     assert np.all(np.isclose(cf.velocity(), vel))
-    
-def test_cmdVelocityWorld_checkIntegrate(setUp):
+
+
+def test_cmdVelocityWorld_checkIntegrate(setUp, pytestconfig):
     allcfs, timeHelper = setUp()
 
     cf = allcfs.crazyflies[0]
     vel = np.ones(3)
-    cf.cmdVelocityWorld(vel, yawRate=0)
-    timeHelper.sleep(1.0)
+    for _ in range(10):
+        cf.cmdVelocityWorld(vel, yawRate=0)
+        timeHelper.sleep(0.1)
+
+    if pytestconfig.getoption("simros"):
+        # TODO: Use simulated time.
+        tolerance = 0.2
+    else:
+        tolerance = 1e-6
 
     pos = cf.initialPosition + vel
-    assert np.all(np.isclose(cf.position(), pos))
+    assert np.all(np.isclose(cf.position(), pos, atol=tolerance))
+
 
 def test_cmdVelocityWorld_disturbance(setUp):
     allcfs, timeHelper = setUp(args="--disturbance 1.0")
@@ -76,6 +90,9 @@ def test_cmdVelocityWorld_disturbance(setUp):
     pos = cf.initialPosition + vel
     assert not np.any(np.isclose(cf.position(), pos))
 
+
+# This is a low-level test of the integrate() method. No need to generalize to ROS.
+@pytest.mark.sync
 def test_sleepResidual(setUp):
     """Verify TimeHelper's time() is consistent with its integration steps."""
     np.random.seed(0)

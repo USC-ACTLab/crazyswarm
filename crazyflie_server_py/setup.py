0fc9cc32169cc835d3440cab3ef08794b4dcb1a8
knmcguire <kimberly@bitcraze.io>
2022-07-20 13:40:27 +0200

    added launch file for testing and fix build issue

--- crazyflie_server_py/setup.py
+++ crazyflie_server_py/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'crazyflie_server_py'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'crazyflie_server_py = crazyflie_server_py:main',
+                'crazyflie_server = crazyflie_server_py.crazyflie_server:main',
         ],
     },
 )

779aec33168e156f94683e7e05fa0f0b71eae10f
knmcguire <kimberly@bitcraze.io>
2022-08-05 15:20:18 +0200

    remove old connected cflib function

--- crazyflie_server_py/crazyflie_server_py/crazyflie_server.py
+++ crazyflie_server_py/crazyflie_server_py/crazyflie_server.py
@@ -54,7 +54,6 @@ class CrazyflieServer(Node):
         self.swarm.all_fully_connected = False
         for link_uri in self.uris:
             self.swarm._cfs[link_uri].cf.fully_connected.add_callback(self._fully_connected)
-            self.swarm._cfs[link_uri].cf.connected.add_callback(self._connected)
             self.swarm._cfs[link_uri].cf.disconnected.add_callback(self._disconnected)
             self.swarm._cfs[link_uri].cf.connection_failed.add_callback(
                 self._connection_failed
@@ -120,8 +119,6 @@ class CrazyflieServer(Node):
                         for link_uri_temp in self.uris:
                             self.swarm._cfs[link_uri_temp].cf.param.set_value(name, parameter.value)
 
-    def _connected(self, link_uri):
-        self.get_logger().info(f" {link_uri} is connected!")
 
     def _disconnected(self, link_uri):
         self.get_logger().info(f" {link_uri} is disconnected!")

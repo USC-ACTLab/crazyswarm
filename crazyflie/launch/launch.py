9895e5079c7cbcafdbed609c3a5eaca754689ec6
knmcguire <kimberly@bitcraze.io>
2022-09-04 19:20:28 +0200

    remove unused import

--- crazyflie/launch/launch.py
+++ crazyflie/launch/launch.py
@@ -4,7 +4,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 from launch.conditions import LaunchConfigurationEquals
 
 

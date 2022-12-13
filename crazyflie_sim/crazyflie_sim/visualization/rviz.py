17a3b116c9ad596daea835e4c4004ea4f3ae68c9
Wolfgang Hoenig <hoenig@tu-berlin.de>
2022-12-13 12:30:32 +0100

    Sim: split backend in backend and visualization
    * This allows to reuse common visualizations across backends.
    
    Related to design in #117.

--- crazyflie_sim/crazyflie_sim/visualization/rviz.py
+++ crazyflie_sim/crazyflie_sim/visualization/rviz.py
@@ -1,30 +1,19 @@
 from geometry_msgs.msg import TransformStamped
 from tf2_ros import TransformBroadcaster
 from rclpy.node import Node
-from rosgraph_msgs.msg import Clock
-from rclpy.time import Time
 
 
-class BackendRviz:
+class Visualization:
 
     def __init__(self, node: Node):
         self.node = node
-        self.clock_publisher = node.create_publisher(Clock, 'clock', 10)
         self.tfbr = TransformBroadcaster(self.node)
-        self.t = 0
-        self.dt = 0.1
 
     def init(self, names, positions):
         self.names = names
 
-    def time(self) -> float:
-        return self.t
-
     def step(self, setpoints):
-        # advance the time
-        self.t += self.dt
-
-        # step the robot itself (here: we just visualize the setpoint)
+        # publish transformation to visualize in rviz
         msgs = []
         for name, setpoint in zip(self.names, setpoints):
             msg = TransformStamped()
@@ -40,9 +29,4 @@ class BackendRviz:
             msg.transform.rotation.w = setpoint.quat[0]
             msgs.append(msg)
         self.tfbr.sendTransform(msgs)
-
-        # publish the current clock
-        clock_message = Clock()
-        clock_message.clock = Time(seconds=self.time()).to_msg()
-        self.clock_publisher.publish(clock_message)
 

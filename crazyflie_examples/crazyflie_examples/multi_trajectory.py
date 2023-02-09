83f47f95dfd12ecd8f7cb69f749867ff50930833
Pablo Robles <pablo.robles@protonmail.com>
2023-02-09 17:30:35 +0100

    examples: safer multi trajectory

--- crazyflie_examples/crazyflie_examples/multi_trajectory.py
+++ crazyflie_examples/crazyflie_examples/multi_trajectory.py
@@ -24,18 +24,18 @@ def main():
         for idx, cf in enumerate(allcfs.crazyflies):
             cf.uploadTrajectory(0, 0, trajs[idx % len(trajs)])
 
-        allcfs.takeoff(targetHeight=1.0, duration=0.1)
-        timeHelper.sleep(0.2)
+        allcfs.takeoff(targetHeight=1.0, duration=2.0)
+        timeHelper.sleep(3.0)
         for cf in allcfs.crazyflies:
-            pos = np.array(cf.initialPosition) + np.array([0, 0, 0])
-            cf.goTo(pos, 0, 1.0)
-        timeHelper.sleep(1.1)
+            pos = np.array(cf.initialPosition) + np.array([0.0, 0.0, 1.0])
+            cf.goTo(pos, 0, 2.0)
+        timeHelper.sleep(2.5)
 
         allcfs.startTrajectory(0, timescale=TIMESCALE)
-        timeHelper.sleep(max([t.duration for t in trajs]) * TIMESCALE + 0.2)
+        timeHelper.sleep(max([t.duration for t in trajs]) * TIMESCALE + 2.0)
 
-        allcfs.land(targetHeight=0.0, duration=0.5)
-        timeHelper.sleep(0.6)
+        allcfs.land(targetHeight=0.06, duration=2.0)
+        timeHelper.sleep(3.0)
 
 
 if __name__ == "__main__":

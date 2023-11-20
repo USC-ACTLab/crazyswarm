fb11fa03fc79e766e8eacdfc2c3bcca97f8040bc
knmcguire <kimberly@bitcraze.io>
2023-11-20 14:00:06 +0100

    add launch file

---
+++ crazyflie_examples/launch/multiranger_simple_mapper_launch.py
@@ -0,0 +1,44 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import yaml
+
+
+def generate_launch_description():
+    # load crazyflies
+    crazyflies_yaml = os.path.join(
+        get_package_share_directory('crazyflie'),
+        'config',
+        'crazyflies.yaml')
+
+    with open(crazyflies_yaml, 'r') as ymlfile:
+        crazyflies = yaml.safe_load(ymlfile)
+
+    server_params = crazyflies
+
+    return LaunchDescription([
+        Node(
+            package='crazyflie',
+            executable='crazyflie_server.py',
+            name='crazyflie_server',
+            output='screen',
+            parameters=[server_params],
+        ),
+        Node(
+            package='crazyflie',
+            executable='vel_mux.py',
+            name='vel_mux',
+            output='screen',
+            parameters=[{'hover_height': 0.3},
+                        {'incoming_twist_topic': '/cmd_vel'},
+                        {'robot_prefix': '/cf1'}]
+        ),
+        #Node(
+        #    package='crazyflie_examples',
+        #    executable='simple_mapper.py',
+        #    name='simple mapper',
+        #    output='screen'
+        #),
+    ])
